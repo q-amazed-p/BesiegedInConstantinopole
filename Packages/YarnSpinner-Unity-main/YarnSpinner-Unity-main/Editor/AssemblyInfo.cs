63729ef675d9d3fc7de26ba2858dbdbb336c087b
q-amazed-p <qamazedp@gmail.com>
2023-12-20 21:55:35 +0100

    Music and Yarn Package

---
+++ Packages/YarnSpinner-Unity-main/YarnSpinner-Unity-main/Editor/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyInformationalVersion("2.4.0")]
+
+[assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
---
+++ Packages/YarnSpinner-Unity-main/YarnSpinner-Unity-main/Samples~/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyInformationalVersion("2.4.0")]
